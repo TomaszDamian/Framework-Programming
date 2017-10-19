9f40f9a953d1bc12a453ca4ceefa505527628ba6
Tomasz Damian <Tom.Sid@wp.pl>
2017-10-19 14:10:43 +0000

    trying to get the layout I want

--- Skilaverkefni 3/Draw.js
+++ Skilaverkefni 3/Draw.js
@@ -1,53 +1,56 @@
-window.onload = function(){
-	var canvas=document.createElement('canvas');
-	var Breakingelement = document.createElement("br")
-	var Rinput = document.createElement("input");
-	var ctx = canvas.getContext("2d");
-	
-	canvas.id='thecanvas';
-	canvas.width='1000';
-	canvas.height='800';
-	canvas.style.border='1px solid red';
-	canvas.addEventListener("mousedown",MouseDown);
-	canvas.addEventListener("mousemove",GetMousePosition);
-	canvas.addEventListener("mouseup",MouseUp);
-	document.body.appendChild(canvas);
-	document.body.appendChild(Breakingelement)
-	document.body.appendChild(Rinput)
-	
-	var IsMouseDown = false;
-	var PrevMouseXpos = 0;
-	var PrevMouseYpos = 0;
-	var MouseXpos = 0;
-	var MouseYpos = 0;
-	
-	function MouseDown(){
-		IsMouseDown = true;
-	}
-	function MouseUp(){
-		IsMouseDown = false;
-	}
+var canvas=document.createElement('canvas');
+/*var para = document.createElement("p");
+var node = document.createTextNode("it is not reccomended that your pen size goes over 5 points");
+para.appendChild(node)*/
+var ctx = canvas.getContext("2d");
 
-	function GetMousePosition(event){
-		PrevMouseXpos = MouseXpos
-		PrevMouseYpos = MouseYpos
-		MouseXpos = event.clientX - 8;
-		MouseYpos = event.clientY - 10;
-	}
+canvas.id='thecanvas';
+canvas.width='1000';
+canvas.height='800';
+canvas.style.border='1px solid red';
+canvas.addEventListener("mousedown",MouseDown);
+canvas.addEventListener("mousemove",GetMousePosition);
+canvas.addEventListener("mouseup",MouseUp);
+document.getElementById('canvasDiv').appendChild(canvas);
+//document.getElementById('canvasDiv').appendChild(para)
 
-	function DrawWithCurosr(prevX,prevY,posX,posY) {
-		ctx.beginPath();
-		ctx.moveTo(prevX, prevY);
-		ctx.lineTo(posX, posY);
-		ctx.strokeStyle = "black";
-		ctx.lineWidth = 2;
-		ctx.stroke();
-		ctx.closePath();
-	}
-	
-	function implement_moment_in_time(){
-		if(IsMouseDown){DrawWithCurosr(PrevMouseXpos,PrevMouseYpos,MouseXpos,MouseYpos)}
-		requestAnimationFrame(implement_moment_in_time);
-	}
-	implement_moment_in_time();
-};
+
+var IsMouseDown = false;
+var PrevMouseXpos = 0;
+var PrevMouseYpos = 0;
+var MouseXpos = 0;
+var MouseYpos = 0;
+
+function MouseDown(){
+	IsMouseDown = true;
+}
+function MouseUp(){
+	IsMouseDown = false;
+}
+
+function GetMousePosition(event){
+	PrevMouseXpos = MouseXpos
+	PrevMouseYpos = MouseYpos
+	MouseXpos = event.clientX;
+	MouseYpos = event.clientY;
+}
+
+function DrawWithCurosr(prevX,prevY,posX,posY) {
+	ctx.beginPath();
+	ctx.moveTo(prevX, prevY);
+	ctx.lineTo(posX, posY);
+	ctx.strokeStyle = "black";
+	ctx.lineWidth = 2;
+	ctx.stroke();
+	ctx.closePath();
+}
+
+function TakeRedColor(){
+	document.getElementById('')
+}
+
+function implement_moment_in_time(){
+	if(IsMouseDown){DrawWithCurosr(PrevMouseXpos,PrevMouseYpos,MouseXpos,MouseYpos)}
+	requestAnimationFrame(implement_moment_in_time);
+}
+implement_moment_in_time();
