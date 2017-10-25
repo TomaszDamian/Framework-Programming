cba39a49ec43edddd746e58c05606f9a904dee58
Tomasz Damian <Tom.Sid@wp.pl>
2017-10-25 13:05:29 +0000

    trying to make image data not crash chrome

--- Skilaverkefni 3/Draw.js
+++ Skilaverkefni 3/Draw.js
@@ -9,9 +9,9 @@ canvas.addEventListener("mousedown",MouseDown);
 canvas.addEventListener("mousemove",GetMousePosition);
 canvas.addEventListener("mouseup",MouseUp);
 document.getElementById('canvasDiv').appendChild(canvas);
-document.getElementById('RedColorNumber').value = 0;
-document.getElementById('GreenColorNumber').value = 0;
-document.getElementById('BlueColorNumber').value = 0;
+document.getElementById('RedColorNumber').value = 1;
+document.getElementById('GreenColorNumber').value = 1;
+document.getElementById('BlueColorNumber').value = 1;
 document.getElementById('BrushSize').value = 1;
 
 
@@ -20,10 +20,12 @@ var PrevMouseXpos = 0;
 var PrevMouseYpos = 0;
 var MouseXpos = 0;
 var MouseYpos = 0;
-var Rcolor = 0;
-var Gcolor = 0;
-var Bcolor = 0;
+var Rcolor = 1;
+var Gcolor = 1;
+var Bcolor = 1;
 var BrushSize = 1;
+var image_data = [];
+var paintedPixels = [];
 
 function MouseDown(){
 	IsMouseDown = true;
@@ -44,11 +46,37 @@ function DrawWithCurosr(prevX,prevY,posX,posY,Rcolor,Gcolor,Bcolor,BrushSize) {
 	ctx.beginPath();
 	ctx.moveTo(prevX, prevY);
 	ctx.lineTo(posX, posY);
-	ctx.strokeStyle = "rgb(" + Rcolor + "," + Gcolor + "," + Bcolor + ")";
+	ctx.strokeStyle = "rgba(" + Rcolor + "," + Gcolor + "," + Bcolor + ",254)";
 	ctx.lineWidth = BrushSize;
 	ctx.stroke();
 	ctx.closePath();
 };
+
+/* dont mind this just future referece
+canvas_array=[[]]
+canvas_array[mousex][mousey]={painted_at:}
+one_pixel={}
+
+slot numer..
+radius
+
+faerd til baka
+
+lista af slot numerum.
+*/
+function getImageData(){
+	image_data=ctx.getImageData(0,0,canvas.width,canvas.height);
+}
+
+function CheckIfPixelIspainted(){
+	for (var counter = 4; counter <= image_data.data.length; counter + 4){
+		if(image_data.data[counter]===254){
+			paintedPixels.append(image_data.data[counter])
+		}
+	}
+	console.log(paintedPixels)
+}
+
 
 function TakeRedColor(){
 	Rcolor = document.getElementById('RedColorNumber').value;
@@ -56,9 +84,9 @@ function TakeRedColor(){
 		document.getElementById('RedColorNumber').value = 255;
 		Rcolor = 255;
 	};
-	if(Rcolor < 0){
-		document.getElementById('RedColorNumber').value = 0;
-		Rcolor = 0;
+	if(Rcolor < 1){
+		document.getElementById('RedColorNumber').value = 1;
+		Rcolor = 1;
 	};
 };
 
@@ -68,9 +96,9 @@ function TakeGreenColor(){
 		document.getElementById('GreenColorNumber').value = 255;
 		Gcolor = 255;
 	};
-	if(Gcolor < 0){
-		document.getElementById('GreenColorNumber').value = 0;
-		Gcolor = 0;
+	if(Gcolor < 1){
+		document.getElementById('GreenColorNumber').value = 1;
+		Gcolor = 1;
 	};
 };
 
@@ -80,9 +108,9 @@ function TakeBlueColor(){
 		document.getElementById('BlueColorNumber').value = 255;
 		Bcolor = 255;
 	};
-	if(Bcolor < 0){
-		document.getElementById('BlueColorNumber').value = 0;
-		Bcolor = 0;
+	if(Bcolor < 1){
+		document.getElementById('BlueColorNumber').value = 1;
+		Bcolor = 1;
 	};
 };
 
@@ -93,19 +121,20 @@ function GetBrushSize(){
 		BrushSize = 6;
 	};
 	if(BrushSize < 1){
-		document.getElementBy('BrushSize').value = 1;
+		document.getElementById('BrushSize').value = 1;
 		BrushSize = 1;
 	};
 };
 
 function RestartColors(){
-	document.getElementById('RedColorNumber').value = 0;
-	document.getElementById('GreenColorNumber').value = 0;
-	document.getElementById('BlueColorNumber').value = 0;
-	Rcolor = 0;
-	Gcolor = 0;
-	Bcolor = 0;
-}
+	document.getElementById('RedColorNumber').value = 1;
+	document.getElementById('GreenColorNumber').value = 1;
+	document.getElementById('BlueColorNumber').value = 1;
+	Rcolor = 1;
+	Gcolor = 1;
+	Bcolor = 1;
+};
+
 function ClearCanvas(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
@@ -115,9 +144,13 @@ function ColorDivChange(Rcolor,Gcolor,Bcolor){
 };
 
 function implement_moment_in_time(){
-	if(IsMouseDown){DrawWithCurosr(PrevMouseXpos,PrevMouseYpos,MouseXpos,MouseYpos,Rcolor,Gcolor,Bcolor,BrushSize)}
+	if(IsMouseDown){
+		DrawWithCurosr(PrevMouseXpos,PrevMouseYpos,MouseXpos,MouseYpos,Rcolor,Gcolor,Bcolor,BrushSize)
+		getImageData()
+	}
 	requestAnimationFrame(implement_moment_in_time);
 	ColorDivChange(Rcolor,Gcolor,Bcolor)
+
 };
 
 implement_moment_in_time();
