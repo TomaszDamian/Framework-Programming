3c6bb4432510d2586ddfac2304c7f280f84a7238
Tomasz Damian <Tom.Sid@wp.pl>
2017-11-10 13:06:14 +0000

    breaking the code up into functions and simplifing everything

--- skil4/Draw.js
+++ skil4/Draw.js
@@ -3,7 +3,7 @@ var SepiaActive = false;
 var BlackAndWhiteActive = false;
 var RedAndwhiteActive = false;
 var isBlurred = false;
-var GradualBlack = false;
+var GradualBlackActive = false;
 var Ypercentage = 1;
 
 var button=document.createElement('button');
@@ -21,6 +21,7 @@ button.addEventListener('click',function(){
 	SepiaActive = !SepiaActive;
 	BlackAndWhiteActive = false;
 	RedAndwhiteActive = false;
+	GradualBlackActive = false;
 });
 
 var button2 = document.createElement('button');
@@ -36,6 +37,7 @@ button2.addEventListener('click',function(){
 	SepiaActive = false;
 	BlackAndWhiteActive = !BlackAndWhiteActive;
 	RedAndwhiteActive = false;
+	GradualBlackActive = false;
 });
 
 var button3 = document.createElement('button');
@@ -51,6 +53,7 @@ button3.addEventListener('click',function(){
 	SepiaActive = false;
 	BlackAndWhiteActive = false;
 	RedAndwhiteActive = !RedAndwhiteActive;
+	GradualBlackActive = false;
 });
 
 var button4 = document.createElement('button');
@@ -78,7 +81,7 @@ button5.addEventListener('click',function(){
 	SepiaActive = false;
 	BlackAndWhiteActive = false;
 	RedAndwhiteActive = false;
-	GradualBlack = !GradualBlack;
+	GradualBlackActive = !GradualBlackActive;
 });
 
 
@@ -133,109 +136,162 @@ function set_canvas_dimensions(){
     canvas.height=parseInt(canvas.width/ratio,10);
     }
 
-function paste_video_on_canvas(){
-    //her faum vid sekunduna sem vid erum a innan i videoinu.
-    var second_in_video=video.currentTime;
-    //einnig er haegt ad lata videoid faerast med thvi ad gera:
-    //video.currentTime=33;
 
-
-
-    //set the size of the rectangle that will be filled and draw the image onto the canvas.
-    canvasp.fillRect(0,0,canvas.width,canvas.height);
-    canvasp.drawImage(video,0,0,canvas.width,canvas.height);
-    
-    var image_data=canvasp.getImageData(0,0,500,500);
-
-    var pixel_object=image_data.data;
-    var slot_quantity=image_data.data.length;
-    for(var cline=0;cline<slot_quantity;cline+=4){
-        
-        //getting rgba values of one pixel from one 2d list
-        //one pixel is 4 different objects in the image_data list
-        //that said first pixel starts on 0 second on 4 third on 8 and so on
-        //that is what the for loop is doing for me and all I do is just get the values manually
-        //by doing +0,+1,+2 and +3
-        //0 being red, 1 being green, 2 being blue and 3 being apacity/opacity 
-        var red=pixel_object[cline+0];
+function MakeBlackAndWhite(pixel_object, image_data){ 
+	var slot_quantity = image_data.data.length
+	for(var cline=0;cline<slot_quantity;cline+=4){
+		var red=pixel_object[cline+0];
         var green=pixel_object[cline+1];
         var blue=pixel_object[cline+2];
-        var opacity=pixel_object[cline+3];
-       
+
+        //total of all colors
         var total = red+green+blue;
         
         //average brigthness of every pixel
         var pixel_average=(total/3);
         
-        //change it into percantage
+        //change it to percantage
         var lignt_percantage = (pixel_average/255);
+
+        var red_filtered_pixel = 255 * lignt_percantage;
+        var green_filtered_pixel = 255 * lignt_percantage;
+        var blue_filtered_pixel = 255 * lignt_percantage;
+		
+		pixel_object[cline+0]= red_filtered_pixel;
+    	pixel_object[cline+1]= green_filtered_pixel;
+    	pixel_object[cline+2]= blue_filtered_pixel;
+
+	};
+	return pixel_object;
+}
+
+function MakeRedAndWhite(pixel_object, image_data){
+	var slot_quantity = image_data.data.length
+	for(var cline=0;cline<slot_quantity;cline+=4){
+		var red=pixel_object[cline+0];
+        var green=pixel_object[cline+1];
+        var blue=pixel_object[cline+2];
+
+        //total of all colors
+        var total = red+green+blue;
         
-        //Make movie red
-        //explanation below
-        if(RedAndwhiteActive){
-	        var red_filtered_pixel = 255 * lignt_percantage
-	        var green_filtered_pixel = 0 * lignt_percantage
-	        var blue_filtered_pixel = 0 * lignt_percantage
-        };
+        //average brigthness of every pixel
+        var pixel_average=(total/3);
+        
+        //change it to percantage
+        var lignt_percantage = (pixel_average/255);
 
-        //sepia filter
-        //explanation below
-        if(SepiaActive){
-       		var red_filtered_pixel = 180 * lignt_percantage
-        	var green_filtered_pixel = 80 * lignt_percantage
-        	var blue_filtered_pixel = 20 * lignt_percantage
-        };
+        var red_filtered_pixel = 255 * lignt_percantage;
+        var green_filtered_pixel = 0 * lignt_percantage;
+        var blue_filtered_pixel = 0 * lignt_percantage;
+		
+		pixel_object[cline+0]= red_filtered_pixel;
+    	pixel_object[cline+1]= green_filtered_pixel;
+    	pixel_object[cline+2]= blue_filtered_pixel;
 
-        //Black and white
-        //black is 255,255,255 and white is 0,0,0
-        //you basically input the highest value you want the pixel to be
-        //as the number that I'll call x so it would go x*light_percantage
-        if(BlackAndWhiteActive){
-        	var red_filtered_pixel = 255 * lignt_percantage
-        	var green_filtered_pixel = 255 * lignt_percantage
-        	var blue_filtered_pixel = 255 * lignt_percantage
-        };
+	};
+	return pixel_object;
+}
 
-        if(GradualBlack){
-        	//byrjum a ad gera okkur svarthvit.
-        	
-        	var red_filtered_pixel = (255 * lignt_percantage)
-        	var green_filtered_pixel = (255 * lignt_percantage)
-        	var blue_filtered_pixel = (255 * lignt_percantage)
-        	
-        	//svo thurfum vid ad fatta.. hvernig rgb eru, midad vid svarthvitt.
-        	//mismunur a r og svarthvitu birtunni.
-        	var dr = (red-pixel_average);
-        	var dg = (green-pixel_average);
-        	var db = (blue-pixel_average);
-        	//svo getum vid profad ad baeta thessu aftur a.. tha a thetta ad verda i lit.
-			red_filtered_pixel+=(dr*Ypercentage);
-			green_filtered_pixel+=(dg*Ypercentage);
-			blue_filtered_pixel+=(db*Ypercentage);
+function MakeSepia(pixel_object, image_data){
+	var slot_quantity = image_data.data.length
+	for(var cline=0;cline<slot_quantity;cline+=4){
+		var red=pixel_object[cline+0];
+        var green=pixel_object[cline+1];
+        var blue=pixel_object[cline+2];
 
+        //total of all colors
+        var total = red+green+blue;
+        
+        //average brigthness of every pixel
+        var pixel_average=(total/3);
+        
+        //change it to percantage
+        var lignt_percantage = (pixel_average/255);
 
-		}
+        var red_filtered_pixel = 180 * lignt_percantage;
+        var green_filtered_pixel = 80 * lignt_percantage;
+        var blue_filtered_pixel = 20 * lignt_percantage;
+		
+		pixel_object[cline+0]= red_filtered_pixel;
+    	pixel_object[cline+1]= green_filtered_pixel;
+    	pixel_object[cline+2]= blue_filtered_pixel;
 
-        //toggles between true and false turning it on an off will work with any filter
-        if(isBlurred){
-        	canvasp.filter = 'blur(3px)'
-    	}
-    	else{
-        	canvasp.filter = 'blur(0px)'
-    	};
+	};
+	return pixel_object;
+}
+function MakeBlurred(canvasp){
+	canvasp.filter = "blur(3px)";
+	return canvasp;
+}
+function Unblur(canvasp){
+	canvasp.filter = "blur(0px)";
+	return canvasp;
+}
 
-        //this is basically done to be able to append the filtered pixels instead of the unfiltered ones
-        if(BlackAndWhiteActive || SepiaActive || RedAndwhiteActive || GradualBlack){
-        	pixel_object[cline+0]= red_filtered_pixel;
-        	pixel_object[cline+1]= green_filtered_pixel;
-        	pixel_object[cline+2]= blue_filtered_pixel;
-    	}
-    	else{
-    		pixel_object[cline+0]= red;
-        	pixel_object[cline+1]= green;
-        	pixel_object[cline+2]= blue;
-    	};
+function MakeGradualBlack(pixel_object, image_data){
+	var slot_quantity = image_data.data.length
+	for(var cline=0;cline<slot_quantity;cline+=4){
+		var red=pixel_object[cline+0];
+        var green=pixel_object[cline+1];
+        var blue=pixel_object[cline+2];
 
+		//total of all colors
+        var total = red+green+blue;
+        
+        //average brigthness of every pixel
+        var pixel_average=(total/3);
+        
+        //change it to percantage
+        var lignt_percantage = (pixel_average/255);
+		
+		//byrjum a ad gera okkur svarthvit.
+    	var red_filtered_pixel = (255 * lignt_percantage);
+    	var green_filtered_pixel = (255 * lignt_percantage);
+    	var blue_filtered_pixel = (255 * lignt_percantage);
+    	
+    	//svo thurfum vid ad fatta.. hvernig rgb eru, midad vid svarthvitt.
+    	//mismunur a r og svarthvitu birtunni.
+    	var dr = (red-pixel_average);
+    	var dg = (green-pixel_average);
+    	var db = (blue-pixel_average);
+    	//svo getum vid profad ad baeta thessu aftur a.. tha a thetta ad verda i lit.
+		red_filtered_pixel+=(dr*Ypercentage);
+		green_filtered_pixel+=(dg*Ypercentage);
+		blue_filtered_pixel+=(db*Ypercentage);
+
+		pixel_object[cline+0]= red_filtered_pixel;
+    	pixel_object[cline+1]= green_filtered_pixel;
+    	pixel_object[cline+2]= blue_filtered_pixel;
+	};
+	return pixel_object;
+}
+
+function paste_video_on_canvas(){
+    //her faum vid sekunduna sem vid erum a innan i videoinu.
+    var second_in_video=video.currentTime;
+    //einnig er haegt ad lata videoid faerast med thvi ad gera:
+    //video.currentTime=33;
+    //set the size of the rectangle that will be filled and draw the image onto the canvas.
+
+    canvasp.fillRect(0,0,canvas.width,canvas.height);
+    canvasp.drawImage(video,0,0,canvas.width,canvas.height);   
+    var image_data=canvasp.getImageData(0,0,500,500);
+    var pixel_object=image_data.data;
+
+    if(SepiaActive){pixel_object = MakeSepia(pixel_object, image_data)};
+    
+    if(BlackAndWhiteActive){pixel_object = MakeBlackAndWhite(pixel_object, image_data)};
+    
+    if(RedAndwhiteActive){pixel_object = MakeRedAndWhite(pixel_object, image_data)};
+    
+    if(isBlurred){canvasp = MakeBlurred(canvasp)}else{canvasp = Unblur(canvasp)};
+
+    if(GradualBlackActive){pixel_object = MakeGradualBlack(pixel_object, image_data)};
+
+    image_data.data=pixel_object;
+    canvasp.putImageData(image_data,0,0);
+    /*
         //both of the below will not work if any filter is on the canvas so be wary of that
         //change the first subtitles 
         if(second_in_video > 20 && second_in_video < 23){
@@ -258,8 +314,5 @@ function paste_video_on_canvas(){
                 pixel_object[cline+2]= 0;
             }
         }
-        }
-    image_data.data=pixel_object;
-
-    canvasp.putImageData(image_data,0,0);
+        }*/
     }
