5c9925ebf305aa488927098eb5a8ec820e75241f
Tomasz <Tom.Sid@wp.pl>
2017-11-15 21:16:25 +0000

    Started draw function and made a clear canvas function (want to finish player class before saturday)

--- Lokaverkefni/Game.js
+++ Lokaverkefni/Game.js
@@ -11,23 +11,41 @@ window.onload = function(){
 	canvas.style.border='1px solid red';
 	document.body.appendChild(canvas)
 
+
+	//I kinda want to split all the fucntions into files
+	//so it doesnt get too cluttered
+	//player class into one and then enemy class into another
+	//then a file with all the universal classes that go like /*function function(x){}*/
+	//then have one master .js file that compiles all of them making it easy to read.
+	//will look into it soon
 	//this is a class for the player defining
 	Player = function(args){
 		var xpos = args.x;
 		var ypos = args.y;
 		var hitPoints = args.hp;
 		var PlayerModelLink = args.PlayerModelLink
+	}
+	
+	Player.prototype.Draw = function(first_argument) {
 		
+	};
+	
+	function canvasClear(){
+		//made to just clear the canvas so the tanks could be redrawn
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	}
-	Player({x:250,y:250,hp:3,PlayerModelLink:"https://i.imgur.com/0njuM9y.png"})
 
+	//RAF is much lighter on the cpu and thats why I'm using that instead of a setTimeout
 	function implement_moment_in_time(){
-	//some code here.
-	//skellir gögnunum aftur á canvasið:
-	//lætur örgjörvann keyra þetta fall aftur við næsta hentuga tækifæri.
+	
+	CanvasClear();
+	Player.Draw();
+
+	//this just call the RAF again and it proccesses when it can
 	requestAnimationFrame( implement_moment_in_time);
-	}
+	};
 	implement_moment_in_time();
+	//calling this player class once in the begging
+	Player({x:250,y:250,hp:3,PlayerModelLink:"https://i.imgur.com/0njuM9y.png"})
 };
-//enemy player model needs to be edited
-//https://i.imgur.com/PpUf1Ih.png link to the 
+//https://i.imgur.com/rsaFhHn.png link to the enemy model
